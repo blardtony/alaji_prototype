5229638d582947a66a34f3ebf088a0b69e2364da
Tony Blard <tonyblard55@gmail.com>
2020-02-14 11:41:30 +0100

    nav bar header and bottom created

---
+++ template/navbar.php
@@ -0,0 +1,20 @@
+<nav class="mb-1 navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container">
+        <img src="https://www.alaji.fr/wp-content/uploads/logo.png" alt="" class="logo">
+        <a class="navbar-brand" href="#">Prototype application</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+        aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/apiMoodle">
+                    Liste
+                    <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
